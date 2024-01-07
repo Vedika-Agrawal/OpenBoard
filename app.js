cbd135a2f7fde7a9a1aec12d42f7c8d59aed8acd
Vedika-Agrawal <vedikaagrawal53040@gmail.com>
2024-01-08 00:58:18 +0530

    completed

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express"); // Access
+const socket = require("socket.io");
+
+const app = express(); //Initialized and server ready
+
+app.use(express.static("public"));//current levelmpe folder hai
+
+let port = process.env.PORT || 5000;
+let server = app.listen(port, () => {
+    console.log("Listening to port" + port);
+})
+
+const cors = require("cors");
+app.use(cors());
+let io = socket(server);//connection baan gya 
+
+io.on("connection", (socket) => {
+    console.log("Made socket connection");
+//     // Received data
+    socket.on("beginPath", (data) => {
+        // data -> data from frontend
+        // Now transfer data to all connected computers
+        io.sockets.emit("beginPath", data);
+    })
+    socket.on("drawStroke", (data) => {
+        io.sockets.emit("drawStroke", data);
+    })
+    socket.on("redoUndo", (data) => {
+        io.sockets.emit("redoUndo", data);
+    })
+})
